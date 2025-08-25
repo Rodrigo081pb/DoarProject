dafa07913b39dc61a49a877e5a4449cb50ece03c
Kauã Rodrigo <147294173+Rodrigo081pb@users.noreply.github.com>
2025-08-24 22:48:44 -0300

    Atualizar o routes.jsx

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-// Exemplo de importação de páginas (adicione suas páginas reais aqui)
 import Home from '../pages/Home/Home';
 
 export default function AppRoutes() {
